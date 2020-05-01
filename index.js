8b19a2fc604a0cb73cb8c9a81389fe48ea175c3f
Ilya Petrenko <gr0x562@gmail.com>
2020-05-01 13:44:47 +0300

    add handlebars

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const exphbs = require('express-handlebars');
 
 const PORT = process.env.PORT || 3333;
 const app = express();
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs'
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
 
 async function start() {
   try {
-    await mongoose.connect('', {
+    await mongoose.connect('mongodb+srv://ilya:1q2w3e4r@cluster0-pcqlp.mongodb.net/todos', {
       useNewUrlParser: true,
       useFindAndModify: false
     });
